1f28771ff361780314faa8e85a195e7df5b63daa
R ansh joseph <70263460+anshjoseph@users.noreply.github.com>
2023-04-02 06:44:03 +0530

    Update script.js
    2423

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+message = document.getElementById('mSec')
+document.getElementById('sendbtn').addEventListener('click',()=>{
+    // console.log(document.getElementById('inpBox').value)
+    // alert(document.getElementById('inpBox').value)
+    
+    let data = document.getElementById('inpBox').value;
+    let para = document.createElement("p")
+    let style = document.createAttribute('style');
+    para.appendChild(document.createTextNode(data))
+    style.value = "float:left;color:white;word-wrap: break-word;";
+    para.setAttributeNode(style)
+    para.classList.add('blue-box')
+    
+    message.appendChild(para)
+})
