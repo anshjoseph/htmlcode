b9caee01c6abca6e7f62956258da23839ac5de1c
R ansh joseph <70263460+anshjoseph@users.noreply.github.com>
2023-04-02 07:04:10 +0530

    Update script.js
    2423-2

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 message = document.getElementById('mSec')
+var cout = 0
 document.getElementById('sendbtn').addEventListener('click',()=>{
     // console.log(document.getElementById('inpBox').value)
     // alert(document.getElementById('inpBox').value)
-    
     let data = document.getElementById('inpBox').value;
     let para = document.createElement("p")
     let style = document.createAttribute('style');
     para.appendChild(document.createTextNode(data))
-    style.value = "float:left;color:white;word-wrap: break-word;";
-    para.setAttributeNode(style)
-    para.classList.add('blue-box')
+    
+    
+    if(cout%2 == 0) {
+        para.classList.add('blue-box');
+        style.value = "float:left;color:white;word-wrap: break-word;";
+        para.setAttributeNode(style)
+    }else{
+        para.classList.add('red-box')
+        style.value = "float:right;color:white;word-wrap: break-word;";
+        para.setAttributeNode(style)
+    }
     
     message.appendChild(para)
+    cout++;
 })
+document.getElementById('btn').addEventListener('click',()=>{
+
+})
+
+
